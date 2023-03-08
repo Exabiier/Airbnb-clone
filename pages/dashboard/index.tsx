9a046127b94fa1bebef6b391e5f824079bb2c8a3
Exabiier <102698808+Exabiier@users.noreply.github.com>
2023-03-08 12:40:09 -0800

    Search Bar is Complete on the header Section

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -5,6 +5,12 @@ export default function DashboardIndexPage() {
     return (
         <div>
             <h1>Hello world</h1>
+            <button disabled>
+                Hello World
+            </button>
+            <p className="blue" data-testid="paragraph-blue">
+                This is a paragraph
+            </p>
         </div>
     )
 
